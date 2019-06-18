c3471400745ddbd10cb12f79d73c854ec9e6e802
Vivek Tadpatri <vivekpisces23@gmail.com>
2019-06-18 17:27:23 +0530

    create_index.sql

--- database/create_index.sql
+++ database/create_index.sql
@@ -7,3 +7,4 @@ CREATE UNIQUE INDEX index_name
 ON table_name ( column1, column2,...);
 
 --A good read => https://stackoverflow.com/questions/3567981/how-do-mysql-indexes-work
+--querying from left most column is very important to take advantage of indexing.
