6296d1988ec23c5d43987916263e4fca2f75ee06
Vivek Tadpatri <vivekpisces23@gmail.com>
2019-06-18 17:25:30 +0530

    create_index.sql

--- database/create_index.sql
+++ database/create_index.sql
@@ -3,3 +3,7 @@
 alter table table_name
 add index column_name;
 
+CREATE UNIQUE INDEX index_name
+ON table_name ( column1, column2,...);
+
+--A good read => https://stackoverflow.com/questions/3567981/how-do-mysql-indexes-work
